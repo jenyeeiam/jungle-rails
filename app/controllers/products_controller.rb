30fffbdaeaecc4f13ac987b09021f598c29a797e
Jen Yee <jenyee1022@yahoo.com>
2016-10-15 01:34:58 +0000

    comment form is working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-
+    @review = @product.reviews.new
     if @product.reviews
       @reviews = @product.reviews
       @reviews
