fa0821c2d683dc62ccc258467bef6c0134ef5ffa
Jen Yee <jenyee1022@yahoo.com>
2016-10-17 21:37:20 +0000

    product_spec checks all of the validations for Product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    before :each do
+      category = Category.create(name: 'clothes')
+      @product = category.products.new(name: 'shirt', price: 100, quantity: 100)
+    end
+
+    it 'should validate presence of name' do
+      @product.name = nil
+      @product.save
+      expect(@product.errors.size).to eq(1)
+      puts @product.errors.full_messages
+    end
+
+    it 'should validate presence of price_cents' do
+      @product.price_cents = nil
+      @product.save
+      # puts @product.inspect
+      # expect(@product.errors.size).to eq(1)
+      expect(@product.price_cents).to_not be_present
+    end
+
+    it 'should validate presence of quantity' do
+      @product.quantity = nil
+      @product.save
+      expect(@product.errors.size).to eq(1)
+    end
+
+  end
+end
